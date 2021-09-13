2748e06ea3c484a0b4a0d98a7a602bc2b408207b
pwndrer4 <53749719+pwndrer4@users.noreply.github.com>
2021-09-13 19:20:38 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-domain=vaiwan.com
+domain="${1}"
 
 echo "以域名${domain}编译ngrok"
 
 cd /usr/local/
 
-yum -y install git golang openssl
+apt-get install -y git golang openssl
 
 sleep 1
 
@@ -15,9 +15,9 @@ sleep 1
 
 cd ngrok/
 openssl genrsa -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=vaiwan.com" -days 5000 -out rootCA.pem
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=${domain}" -days 5000 -out rootCA.pem
 openssl genrsa -out tunnel.key 2048
-openssl req -new -key tunnel.key -subj "/CN=tunnel.vaiwan.com" -out tunnel.csr
+openssl req -new -key tunnel.key -subj "/CN=tunnel.${domain}" -out tunnel.csr
 openssl x509 -req -in tunnel.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out tunnel.crt -days 5000
 cp rootCA.pem assets/client/tls/ngrokroot.crt
 cp tunnel.crt assets/server/tls/snakeoil.crt
@@ -25,8 +25,17 @@ cp tunnel.key assets/server/tls/snakeoil.key
 echo 'generate key ok !'
 
 sleep 1
+
+# 编译生成linux 64bit(mac可以直接用)版本
+GOOS=linux GOARCH=amd64 make release-all
+# 编译生成linux 32bit(mac可以直接用)版本
+GOOS=linux GOARCH=386 make release-all
 # 编译生成linux arm(mac可以直接用)版本
 GOOS=linux GOARCH=arm make release-all
+# 编译生成windows 64bit版本
+GOOS=windows GOARCH=amd64 make release-all
+# 编译生成windows 32bit版本
+GOOS=windows GOARCH=386 make release-all 
 echo 'build ok !'
 
 sleep 1
@@ -39,7 +48,7 @@ ln -s "${ngrokd}" /usr/bin
 
 sleep 1
 
-echo "server_addr: tunnel.vaiwan.com:443" >> bin/ngrok.yml
+echo "server_addr: tunnel.${domain}:4443" >> bin/ngrok.yml
 echo "trust_host_root_certs: false"       >> bin/ngrok.yml
 
 echo "generate ngrok.yml ok !"
